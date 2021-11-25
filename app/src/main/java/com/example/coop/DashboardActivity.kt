8a60258e6194edb83c99d0e6d0104a8bc8224309
Param-Bhatt <f20180862@hyderabad.bits-pilani.ac.in>
2021-11-25 16:42:48 +0530

    Feature update : Static sidepanel works

--- app/src/main/java/com/example/coop/DashboardActivity.kt
+++ app/src/main/java/com/example/coop/DashboardActivity.kt
@@ -4,18 +4,44 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.bumptech.glide.Glide
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
 
     private lateinit var mAuth: FirebaseAuth
+    lateinit var toggle : ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> Toast.makeText(applicationContext, "Clicked home", Toast.LENGTH_SHORT).show()
+                R.id.nav_settings -> Toast.makeText(applicationContext, "Clicked settings", Toast.LENGTH_SHORT).show()
+                R.id.delete -> Toast.makeText(applicationContext, "Clicked delete", Toast.LENGTH_SHORT).show()
+                R.id.topic_share -> Toast.makeText(applicationContext, "Topic share", Toast.LENGTH_SHORT).show()
+
+            }
+            true
+        }
         mAuth = FirebaseAuth.getInstance()
         val currentUser = mAuth.currentUser
         Log.d("User", "$currentUser")
@@ -31,5 +57,12 @@ class DashboardActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }

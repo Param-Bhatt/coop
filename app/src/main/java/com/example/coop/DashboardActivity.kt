75028486774953e2a8af3a026508a7bd601543ac
Param-Bhatt <f20180862@hyderabad.bits-pilani.ac.in>
2021-11-26 20:33:08 +0530

    Feature update: Sidepanel topics updated to respective user topics

--- app/src/main/java/com/example/coop/DashboardActivity.kt
+++ app/src/main/java/com/example/coop/DashboardActivity.kt
@@ -14,6 +14,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.bumptech.glide.Glide
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_dashboard.*
@@ -63,7 +64,8 @@ class DashboardActivity : AppCompatActivity() {
         Glide.with(this).load(currentUser?.photoUrl).into(navView.getHeaderView(0).findViewById(R.id.profile_image_side) as ImageView?)
 
         var collectionPath = "users"
-        db.collection(collectionPath)
+        val query: Query = db.collection("users").whereEqualTo("name", currentUser?.displayName.toString())
+        query
             .get()
             .addOnSuccessListener { userResult ->
                 for(document in userResult){
